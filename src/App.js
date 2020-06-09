3a08a2a5621e179fc3434eda787ed1c755a87240
Higor Correa <higor_manoel2@hotmail.com>
2020-06-09 12:56:21 -0300

    Template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo-hbsis.svg'
+import { Row, Col, Navbar } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Row className="header bg-hbsis">
+        <Col lg={4} md={4}>
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
+        </Col>
+      </Row>
+      <Row className="full-height">
+        <Col className="menu full-height bg-hbsis">
+          <Navbar bg="hbsis">
+            <Navbar.Brand href="#home" className="menu-link">Home</Navbar.Brand>
+          </Navbar>
+          <Navbar bg="hbsis">
+            <Navbar.Brand href="#pessoa" className="menu-link">Pessoa</Navbar.Brand>
+          </Navbar>
+        </Col>
+        <Col md={11} >
+          Olar
+        </Col>
+      </Row>
+    </>
   );
 }
 
