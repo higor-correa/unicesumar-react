b5f08a7be8d96f80558bd4e89adf7033da26cec0
Higor Correa <higor_manoel2@hotmail.com>
2020-06-09 16:58:24 -0300

    Removendo código para handson

--- src/screens/template/index.js
+++ src/screens/template/index.js
@@ -1,13 +1,11 @@
 import logo from 'assets/logo-hbsis.svg';
-import Routes from 'configs/routes';
 import React from 'react';
 import { Col, Navbar, Row } from 'react-bootstrap';
-import { BrowserRouter, Link } from 'react-router-dom';
 import './index.css';
 
 export default function Template() {
     return (
-        <BrowserRouter>
+        <>
             <Row className="header bg-hbsis">
                 <Col lg={4} md={4}>
                     <div className="logo">
@@ -19,19 +17,19 @@ export default function Template() {
                 <Col className="menu full-height bg-hbsis">
                     <Navbar bg="hbsis" variant="dark">
                         <Navbar.Brand className="menu-link">
-                            <Link to="/home" className="navbar-brand">Home</Link>
+                            Home
                         </Navbar.Brand>
                     </Navbar>
                     <Navbar bg="hbsis" variant="dark">
                         <Navbar.Brand className="menu-link">
-                            <Link to="/pessoas" className="navbar-brand">Pessoas</Link>
+                            Pessoas
                         </Navbar.Brand>
                     </Navbar>
                 </Col>
                 <Col md={11} className="content" >
-                    <Routes />
+                    <div>Olar</div>
                 </Col>
             </Row>
-        </BrowserRouter>
+        </>
     );
 }
